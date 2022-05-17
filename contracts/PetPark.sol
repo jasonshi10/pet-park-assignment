9119f321344b03329286c657f787334d591d6d48
jasonshi10 <jasonxinshi@pm.me>
2022-05-16 23:07:04 -0700

    update petpark

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -4,4 +4,41 @@ pragma solidity ^0.8.0;
 
 contract PetPark {
 
+    uint AnimalType;
+    uint Age;
+    uint Gender;
+    uint Count;
+    address contractOwner;
+
+    event Added(uint AnimalType, uint AnimalCount);
+    event Borrowed(uint AnimalType);
+    event Returned(uint AnimalType);
+
+    constructor() {
+    contractOwner = msg.sender;
+    }
+
+    struct AnimalType {
+        string Animal;
+        uint number;
+    }
+
+    Animals[] animals;
+
+    Animals Fish = Animals("Fish", 1)
+
+  function add(AnimalType, Count) {
+    require(msg.sender == contractOwner, "Not an owner");
+    require(AnimalType "Invalid animal");
+
+    emit Added(AnimalType, AnimalCount);
+  };
+
+  function borrow(Age, Gender, AnimalType) {
+    emit Borrowed(AnimalType);
+  };
+
+  function giveBackAnimal() {
+    emit Returned(AnimalType);
+  };
 }
